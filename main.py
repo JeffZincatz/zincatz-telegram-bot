d4219cbfa9e168b99642bbc1921c148ec4d159ff
JeffZincatz <jeffzincatz@gmail.com>
2023-12-26 11:07:33 +0800

    Tidy up

--- main.py
+++ main.py
@@ -20,65 +20,41 @@ logging.basicConfig(
 req = Request()
 scraper = Scraper()
 
+
 '''
-Handlers
+Messages
 '''
+def get_start_message():
+  return "Zincatz's M.League Infoboard\nKindly wait a few seconds for responses."
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await context.bot.send_message(
-      chat_id=update.effective_chat.id,
-      text=
-      "Welcome to the Zincatz's M.League info Bot!\nt.me/zincatz_bot\nUse /help to view all available commands."
-  )
-
-
-async def opponent(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  match_info = scraper.get_match_info()
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="\n".join(match_info))
-
-
-async def team_ranking_regular(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  team_ranking = scraper.get_regualr_team_rank()
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="\n".join(team_ranking))
-
-async def personal_score_regular(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  personal_score = scraper.get_personal_score()
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="\n".join(personal_score))
-
-async def personal_highest_regular(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  personal_highest = scraper.get_personal_highest()
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="\n".join(personal_highest))
-
-async def last_avoid_rate_regular(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  last_avoid_rate = scraper.get_last_avoid_rate()
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text="\n".join(last_avoid_rate))
-
-async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text("Zincatz's M.League Infoboard", reply_markup=await menu_keyboard())
-
-def menu_handler_builder(message:str):
-  async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(message, reply_markup=await menu_keyboard())
-  return menu_handler
-
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  help_message = "\n".join([
-      "Available commands:", "/help - Display this help message",
-      "/opponent - Display the latest opponent information",
-      "/regular - Display the latest regular season team ranking",
-      "/personal_score - Display personal score ranking",
-      "/personal_highest - Display personal highest ranking",
-      "/last_avoid_rate - Display the last avoid rate ranking"
+def get_help_message():
+  return "\n".join([
+      "Available commands:",
+      "/help - Display this help message",
+      "/start - Return to start menu"
   ])
 
-  # Send the help message to the user
-  await context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=help_message)
+def get_opponent_message():
+  return '\n'.join(scraper.get_match_info())
+
+def get_regular_team_rank_message():
+  return '\n'.join(scraper.get_regualr_team_rank())
+
+def get_personal_score_message():
+  return '\n'.join(scraper.get_personal_score())
+
+def get_personal_highest_message():
+  return '\n'.join(scraper.get_personal_highest())
+
+def get_last_avoid_rate_message():
+  return '\n'.join(scraper.get_last_avoid_rate())
+
+'''
+Command handlers
+'''
+async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await update.message.reply_text(get_start_message(), reply_markup=await menu_keyboard())
+
 
 '''
 Callback query handler for menu
@@ -90,39 +66,32 @@ def get_menu_callback_query_handler(message:str):
     await query.edit_message_text(text=message, reply_markup=await menu_keyboard())
   return menu_handler
 
-def help_menu():
-  message = "\n".join([
-      "Available commands:", "/help - Display this help message",
-      "/opponent - Display the latest opponent information",
-      "/regular - Display the latest regular season team ranking",
-      "/personal_score - Display personal score ranking",
-      "/personal_highest - Display personal highest ranking",
-      "/last_avoid_rate - Display the last avoid rate ranking"
-  ])
+def start_menu():
+  message = get_start_message()
   return get_menu_callback_query_handler(message)
 
-def default_menu():
-  message = "Zincatz's M.League Infoboard"
+def help_menu():
+  message = get_help_message()
   return get_menu_callback_query_handler(message)
 
 def opponent_menu():
-  message = '\n'.join(scraper.get_match_info())
+  message = get_opponent_message()
   return get_menu_callback_query_handler(message)
 
 def regular_menu():
-  message = '\n'.join(scraper.get_regualr_team_rank())
+  message = get_regular_team_rank_message()
   return get_menu_callback_query_handler(message)
 
 def personal_score_menu():
-  message = '\n'.join(scraper.get_personal_score())
+  message = get_personal_score_message()
   return get_menu_callback_query_handler(message)
 
 def personal_highest_menu():
-  message = '\n'.join(scraper.get_personal_highest())
+  message = get_personal_highest_message()
   return get_menu_callback_query_handler(message)
 
 def last_avoid_rate_menu():
-  message = '\n'.join(scraper.get_last_avoid_rate())
+  message = get_last_avoid_rate_message()
   return get_menu_callback_query_handler(message)
 
 
@@ -131,8 +100,8 @@ Keyboard
 '''
 async def menu_keyboard():
   keyboard = [
-    [InlineKeyboardButton('Help', callback_data='help'),
-     InlineKeyboardButton('Menu', callback_data='menu')],
+    [InlineKeyboardButton('Start', callback_data='start'),
+     InlineKeyboardButton('Help', callback_data='help')],
     [InlineKeyboardButton('Upcoming Opponents', callback_data='opponent')],
     [InlineKeyboardButton('Team Ranking (regular season)', callback_data='regular')],
     [InlineKeyboardButton('Personal Score Ranking', callback_data='personal_score')],
@@ -151,18 +120,10 @@ if __name__ == '__main__':
   TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
   
   application = ApplicationBuilder().token(TOKEN).build()
-
-  add_command_handler(application, 'start', start)
-  add_command_handler(application, 'help', help)
-  add_command_handler(application, 'opponent', opponent)
-  add_command_handler(application, 'regular', team_ranking_regular)
-  add_command_handler(application, 'personal_score', personal_score_regular)
-  add_command_handler(application, 'personal_highest', personal_highest_regular)
-  add_command_handler(application, 'last_avoid_rate', last_avoid_rate_regular)
   
-  add_command_handler(application, 'menu', menu_handler)
+  application.add_handler(CommandHandler('start', menu_handler))
   application.add_handler(CallbackQueryHandler(help_menu(), pattern='help'))
-  application.add_handler(CallbackQueryHandler(default_menu(), pattern='menu'))
+  application.add_handler(CallbackQueryHandler(start_menu(), pattern='start'))
   application.add_handler(CallbackQueryHandler(opponent_menu(), pattern='opponent'))
   application.add_handler(CallbackQueryHandler(regular_menu(), pattern='regular'))
   application.add_handler(CallbackQueryHandler(personal_score_menu(), pattern='personal_score'))
