d7350256a0836f3b7c81903052f737572fb0ac09
JeffZincatz <jeffzincatz@gmail.com>
2023-12-27 16:44:52 +0800

    Fix bug for menu button not updating text

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, error
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 from request import Request
 from scrape_bs import Scraper
 
@@ -17,6 +17,9 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
 req = Request()
 scraper = Scraper()
 
@@ -25,13 +28,12 @@ scraper = Scraper()
 Messages
 '''
 def get_start_message():
-  return "Zincatz's M.League Infoboard\nKindly wait a few seconds for responses."
+  return f"Zincatz's M.League Infoboard\nKindly wait a few seconds for responses."
 
 def get_help_message():
   return "\n".join([
       "Available commands:",
-      "/help - Display this help message",
-      "/start - Return to start menu"
+      "/start - Display start menu"
   ])
 
 def get_opponent_message():
@@ -49,56 +51,11 @@ def get_personal_highest_message():
 def get_last_avoid_rate_message():
   return '\n'.join(scraper.get_last_avoid_rate())
 
-'''
-Command handlers
-'''
-async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-  await update.message.reply_text(get_start_message(), reply_markup=await menu_keyboard())
-
-
-'''
-Callback query handler for menu
-'''
-def get_menu_callback_query_handler(message:str):
-  async def menu_handler(update:Update, context:ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(text=message, reply_markup=await menu_keyboard())
-  return menu_handler
-
-def start_menu():
-  message = get_start_message()
-  return get_menu_callback_query_handler(message)
-
-def help_menu():
-  message = get_help_message()
-  return get_menu_callback_query_handler(message)
-
-def opponent_menu():
-  message = get_opponent_message()
-  return get_menu_callback_query_handler(message)
-
-def regular_menu():
-  message = get_regular_team_rank_message()
-  return get_menu_callback_query_handler(message)
-
-def personal_score_menu():
-  message = get_personal_score_message()
-  return get_menu_callback_query_handler(message)
-
-def personal_highest_menu():
-  message = get_personal_highest_message()
-  return get_menu_callback_query_handler(message)
-
-def last_avoid_rate_menu():
-  message = get_last_avoid_rate_message()
-  return get_menu_callback_query_handler(message)
-
 
 '''
 Keyboard
 '''
-async def menu_keyboard():
+def menu_keyboard():
   keyboard = [
     [InlineKeyboardButton('Start', callback_data='start'),
      InlineKeyboardButton('Help', callback_data='help')],
@@ -111,8 +68,74 @@ async def menu_keyboard():
   return InlineKeyboardMarkup(keyboard)
 
 
-def add_command_handler(application:Application, handle_name: str, handler):
-  application.add_handler(CommandHandler(handle_name, handler))
+'''
+Command handlers
+'''
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await update.message.reply_text(get_start_message(), reply_markup=menu_keyboard())
+
+
+'''
+Callback query handlers for menu
+'''
+async def help_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_help_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+
+
+async def start_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_start_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+
+async def opponent_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_opponent_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+
+async def regular_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_regular_team_rank_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+  
+async def personal_score_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_personal_score_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+
+async def personal_highest_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_personal_highest_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
+
+async def last_avoid_rate_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  query = update.callback_query
+  await query.answer()
+  try:
+    await query.edit_message_text(text=get_last_avoid_rate_message(), reply_markup=menu_keyboard())
+  except error.BadRequest as e:
+    logging.info(f"{e}")
 
 
 if __name__ == '__main__':
@@ -121,14 +144,14 @@ if __name__ == '__main__':
   
   application = ApplicationBuilder().token(TOKEN).build()
   
-  application.add_handler(CommandHandler('start', menu_handler))
-  application.add_handler(CallbackQueryHandler(help_menu(), pattern='help'))
-  application.add_handler(CallbackQueryHandler(start_menu(), pattern='start'))
-  application.add_handler(CallbackQueryHandler(opponent_menu(), pattern='opponent'))
-  application.add_handler(CallbackQueryHandler(regular_menu(), pattern='regular'))
-  application.add_handler(CallbackQueryHandler(personal_score_menu(), pattern='personal_score'))
-  application.add_handler(CallbackQueryHandler(personal_highest_menu(), pattern='personal_highest'))
-  application.add_handler(CallbackQueryHandler(last_avoid_rate_menu(), pattern='last_avoid_rate'))
-
+  application.add_handler(CommandHandler('start', callback=start_handler))
+  application.add_handler(CallbackQueryHandler(help_menu_handler, pattern='help'))
+  application.add_handler(CallbackQueryHandler(start_menu_handler, pattern='start'))
+  application.add_handler(CallbackQueryHandler(opponent_menu_handler, pattern='opponent'))
+  application.add_handler(CallbackQueryHandler(regular_menu_handler, pattern='regular'))
+  application.add_handler(CallbackQueryHandler(personal_score_menu_handler, pattern='personal_score'))
+  application.add_handler(CallbackQueryHandler(personal_highest_menu_handler, pattern='personal_highest'))
+  application.add_handler(CallbackQueryHandler(last_avoid_rate_menu_handler, pattern='last_avoid_rate'))
+  
   application.run_polling()
   
